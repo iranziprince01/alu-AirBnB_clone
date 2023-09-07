9b767d8e8587c5b5709122db3f19c183d14d3278
iranziprince01 <p.iranzi@alustudent.com>
2023-09-07 05:11:28 -0700

    amenity file

--- models/amenity.py
+++ models/amenity.py
@@ -17,4 +17,3 @@ class Amenity(BaseModel, Base):
         place_amenities = relationship('Place',
                                        secondary='place_amenity',
                                        back_populates='amenities')
-
